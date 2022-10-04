71a0c70cdb738c98d2e76a60de784e569da856b5
px64vk12 <px64vk12@naver.com>
2022-10-05 01:24:33 +0900

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,90 +16,21 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  void _showDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text("Alert!!"),
-          content: Text("You are awesome!"),
-          actions: [
-            MaterialButton(
-              child: Text("OK"),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    // MaterialApp  with debugShowCheckedModeBanner
-    // false and title.
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // scaffold with appbar
       home: Scaffold(
-        // appbar with title text
-        appBar: AppBar(
-          title: Text('AppBar Popup Menu Button'),
-          // in action widget we have PopupMenuButton
-          actions: [
-            PopupMenuButton<int>(
-              itemBuilder: (context) => [
-                // PopupMenuItem 1
-                PopupMenuItem(
-                  value: 1,
-                  // row with 2 children
-                  child: Row(
-                    children: [
-                      Icon(Icons.star),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text("Get The App")
-                    ],
-                  ),
-                ),
-                // PopupMenuItem 2
-                PopupMenuItem(
-                  value: 2,
-                  // row with two children
-                  child: Row(
-                    children: [
-                      Icon(Icons.chrome_reader_mode),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text("About")
-                    ],
-                  ),
-                ),
-              ],
-              offset: Offset(0, 100),
-              color: Colors.grey,
-              elevation: 2,
-              // on selected we show the dialog box
-              onSelected: (value) {
-                // if value 1 show dialog
-                if (value == 1) {
-                  _showDialog(context);
-                  // if value 2 show dialog
-                } else if (value == 2) {
-                  _showDialog(context);
-                }
-              },
-            ),
-          ],
-        ),
+        appBar: AppBar(),
         // body with centered text
         body: Center(
-          child: Text("Press the 3 Point Button Up!"),
-        ),
+            child: ListView(
+          shrinkWrap: true,
+          children: <Widget>[
+            Lottie.network(
+                'https://raw.githubusercontent.com/xvrh/lottie-flutter/master/example/assets/Mobilo/A.json'),
+          ],
+        )),
       ),
     );
   }
