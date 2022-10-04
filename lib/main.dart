6cf102150925e00329e52f74fca69b45a7bae18c
px64vk12 <px64vk12@naver.com>
2022-10-05 01:13:48 +0900

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,103 @@
+/// Copyright 2022. ⓒ DevStory.co.kr All rights reserved.
+
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  void _showDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Alert!!"),
+          content: Text("You are awesome!"),
+          actions: [
+            MaterialButton(
+              child: Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
+    // MaterialApp  with debugShowCheckedModeBanner
+    // false and title.
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      // scaffold with appbar
       home: Scaffold(
-        body: Center(
-          child: Container(
-            width: 200,
-            height: 200,
-            // color : Colors.amber, // decoration와 함께 사용 불
-            decoration: BoxDecoration(
-              color: Colors.amber, // decoration 안에 color를 활용해주세요.
-              borderRadius: BorderRadius.circular(50), // 모서리를 둥글게
-              border: Border.all(color: Colors.blue, width: 5), // 테두리
-              boxShadow: [
-                BoxShadow(
-                  // 그림자 효과
-                  color: Colors.grey.withOpacity(0.7), // 투명도 70% 회색
-                  spreadRadius: 5, // 퍼짐 효과
-                  blurRadius: 7, // 뭉갬 효과
-                  offset: Offset(5, 5), // 그림자를 우측 5 아래 5만큼 이동
+        // appbar with title text
+        appBar: AppBar(
+          title: Text('AppBar Popup Menu Button'),
+          // in action widget we have PopupMenuButton
+          actions: [
+            PopupMenuButton<int>(
+              itemBuilder: (context) => [
+                // PopupMenuItem 1
+                PopupMenuItem(
+                  value: 1,
+                  // row with 2 children
+                  child: Row(
+                    children: [
+                      Icon(Icons.star),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text("Get The App")
+                    ],
+                  ),
+                ),
+                // PopupMenuItem 2
+                PopupMenuItem(
+                  value: 2,
+                  // row with two children
+                  child: Row(
+                    children: [
+                      Icon(Icons.chrome_reader_mode),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text("About")
+                    ],
+                  ),
                 ),
               ],
+              offset: Offset(0, 100),
+              color: Colors.grey,
+              elevation: 2,
+              // on selected we show the dialog box
+              onSelected: (value) {
+                // if value 1 show dialog
+                if (value == 1) {
+                  _showDialog(context);
+                  // if value 2 show dialog
+                } else if (value == 2) {
+                  _showDialog(context);
+                }
+              },
             ),
-            alignment: Alignment.center,
-            child: Text(
-              "hi",
-              style: TextStyle(fontSize: 50),
-            ),
-          ),
+          ],
+        ),
+        // body with centered text
+        body: Center(
+          child: Text("Press the 3 Point Button Up!"),
         ),
       ),
     );
