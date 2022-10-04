43db8297689310eb4461840964514baecd4f1ea5
px64vk12 <px64vk12@naver.com>
2022-10-05 00:09:25 +0900

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,41 @@
-/// 1. 터미널에 flutter pub add shared_preferences
-/// 2.
-///
-///
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-void save_pref(String value) async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  prefs.setString('text', value);
-}
-
-Future<String> load_pref() async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  String stringValue = prefs.getString('text') ?? '';
-  return stringValue;
-}
-
-class _MyAppState extends State<MyApp> {
-  TextEditingController editController = new TextEditingController();
-  String text = "";
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(),
-        body: Container(
-          width: double.infinity,
-          color: Colors.white,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              // 글자 출력
-              Text("저장된 값"),
-              Text('$text'),
-
-              TextField(
-                controller: editController,
-              ),
-
-              ElevatedButton(
-                onPressed: () {
-                  save_pref(editController.text);
-                  print(editController.text);
-                },
-                child: Text('저장하기!'),
-              ),
-
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    load_pref().then((val) {
-                      // int가 나오면 해당 값을 출력
-                      text = val;
-                    }).catchError((error) {
-                      // error가 해당 에러를 출력
-                      print('error: $error');
-                    });
-                  });
-                  print(text);
-                },
-                child: Text('불러오기!'),
-              ),
-            ],
+        body: Center(
+          child: Container(
+            width: 200,
+            height: 200,
+            // color : Colors.amber, // decoration와 함께 사용 불
+            decoration: BoxDecoration(
+              color: Colors.amber, // decoration 안에 color를 활용해주세요.
+              borderRadius: BorderRadius.circular(50), // 모서리를 둥글게
+              border: Border.all(color: Colors.blue, width: 5), // 테두리
+              boxShadow: [
+                BoxShadow(
+                  // 그림자 효과
+                  color: Colors.grey.withOpacity(0.7), // 투명도 70% 회색
+                  spreadRadius: 5, // 퍼짐 효과
+                  blurRadius: 7, // 뭉갬 효과
+                  offset: Offset(5, 5), // 그림자를 우측 5 아래 5만큼 이동
+                ),
+              ],
+            ),
+            alignment: Alignment.center,
+            child: Text(
+              "hi",
+              style: TextStyle(fontSize: 50),
+            ),
           ),
         ),
       ),
