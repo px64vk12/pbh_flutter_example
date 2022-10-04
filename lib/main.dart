706d1427957a6e5d8ec0fc55475493e7b340f92b
px64vk12 <px64vk12@naver.com>
2022-10-04 22:30:20 +0900

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  bool isChecked = false;
+  double sliderValue = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,49 +28,27 @@ class _MyAppState extends State<MyApp> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              // 글자 출력
-              Text("안녕하세요"),
-
-              // 텍스트 입력
-              TextField(
-                autofocus: true, // 자동 포커스
-                onChanged: (text) {
-                  // 텍스트 변경시 실행되는 함수
-                  print(text);
-                },
-                onSubmitted: (text) {
-                  // Enter를 누를 때 실행되는 함수
-                  print("on submitted : $text");
+            children: <Widget>[
+              Switch(
+                  value: isChecked,
+                  onChanged: (bool value) {
+                    // This is called when the user toggles the switch.
+                    setState(() {
+                      isChecked = value;
+                    });
+                    print(value);
+                  }),
+              Slider(
+                value: sliderValue,
+                max: 100,
+                divisions: 5,
+                label: sliderValue.round().toString(),
+                onChanged: (double value) {
+                  setState(() {
+                    sliderValue = value;
+                  });
                 },
               ),
-
-              // 버튼 만들기
-              // ElevatedButton : 네모난 기본 버튼
-              ElevatedButton(
-                onPressed: () {
-                  print("Elevated Button 클릭");
-                },
-                child: Text('Elevated Button'),
-              ),
-              // TextButton : 텍스트를 버튼으로 만들 수 있습니다. 하이퍼 링크
-              TextButton(
-                onPressed: () {
-                  print("Text Button 클릭");
-                },
-                child: Text('Text Button'),
-              ),
-              // IconButton : 아이콘도 버튼으로 만들 수 있습니다.
-              IconButton(
-                onPressed: () {
-                  print("Icon Button 클릭");
-                },
-                icon: Icon(Icons.add),
-              ),
-
-              // Image : 이미지 링크를 가져옵니다.
-              Image.network(
-                  "https://upload.wikimedia.org/wikipedia/ko/thumb/2/24/Lenna.png/220px-Lenna.png")
             ],
           ),
         ),
